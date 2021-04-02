4d21f5266de666c5112fc6937bf4b90c036bc067
fazufi <fazufi10@gmail.com>
2021-04-02 21:46:52 +0700

    react-upload-file

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [image, setImage] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const uploadImage = async e => {
+    const files = e.target.files
+    const data = new FormData()
+    data.append('file', files[0])
+    data.append('upload_preset', 'darwin')
+    setLoading(true)
+    const res = await fetch(
+      'http://localhost:3006/baru',
+      {
+        method: 'POST',
+        body: data
+      }
+    )
+    const file = await res.json()
+
+    setImage(file.secure_url)
+    setLoading(false);
+    console.log([image])
+  }
+
+  return (
+    
+    <div className="App">
+      
+      <h1>Upload Image</h1>
+      <input
+        type="file"
+        name="file"
+        placeholder="Upload an image"
+        onChange={uploadImage}
+      />
+      {loading ? (
+        <h3>Loading...</h3>
+      ) : (
+        <img src={image} style={{ width: '200px', height: '200px' }} alt="heeheh" />
+      )}
+    </div>
+  )
+}
+
+export default App
